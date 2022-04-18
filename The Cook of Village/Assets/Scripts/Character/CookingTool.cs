0dd26cba22e41d4b20b5039983bef89d4419e810
WIN10PC\User <chahg0000@gmail.com>
2022-04-18 18:59:33 +0900

    CookingTool

--- The Cook of Village/Assets/Scripts/Character/CookingTool.cs
+++ The Cook of Village/Assets/Scripts/Character/CookingTool.cs
@@ -13,6 +13,8 @@ public class CookingTool : MonoBehaviour
     public List<int> ingredientList = new List<int>();
     public MaterialInfos currentMaterialInTool; //이걸로 리스트를 아예 만들어서 3개Infos를 다 저장해놓을지 ID만 3개 저장해놓을지 추후 수정
 
+    public FoodInfos foodInfos;
+
     public void PutIngredient() //이걸 현재 들고있는게 null이 아닐때만 실행시켜주면 되는데 혹시몰라서 한번 더 조건문 넣음
     {
 
@@ -33,7 +35,8 @@ public class CookingTool : MonoBehaviour
 
     public void Cook()
     {
-        //FoodData.Instance.foodTool[0].foodInfos[0].
+        foodInfos = FoodData.Instance.Recipe[ingredientList];
+        Debug.Log(foodInfos.Name);
     }
 
 }
