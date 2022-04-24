e71f7b6cd78518a9a375c99421d7da81ec57968e
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-04-24 16:37:19 +0900

    Feat : Chair 번호 랜덤 출력

---
+++ The Cook of Village/Assets/Scripts/NPC/ChairCheck.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChairCheck : MonoBehaviour
+{
+    public List<GameObject> Chair = new List<GameObject>();
+
+    private void Awake()
+    {
+        Chair = GameObject.FindGameObjectsWithTag("Chair").ToList();
+
+    }
+
+    private int ChairNum()
+    {
+        return Random.Range(0, Chair.Count);
+    }
+}
