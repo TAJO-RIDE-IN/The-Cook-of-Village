eb8a257aa5e3f75e4924db4e117f5a636c999818
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-06-05 03:44:34 +0900

    Feat : Today Text 변경 기능 추가

--- The Cook of Village/Assets/Scripts/UI/DisplayUI.cs
+++ The Cook of Village/Assets/Scripts/UI/DisplayUI.cs
@@ -9,21 +9,48 @@ public class DisplayUI : MonoBehaviour, IObserver<GameData>
     public Text Money;
     public Text TimeText;
     public Text DayText;
+    public Text TodayText;
 
     private int[] Time(float currentTime)
     {
         int[] time = new int[2];
         time[0] = (int) currentTime / 60;  //hour
-        time[1] = (int) currentTime % 60;
+        time[1] = (int) currentTime % 60 / 10;
         return time;
     }
 
     public void ChangeDisplay(float time, float day, int money)
     {
-        TimeText.text = Time(time)[0] + " : " + Time(time)[1];
+        TimeText.text = Time(time)[0] + " : " + Time(time)[1]+"0";
         DayText.text = "Day" + day.ToString();
         Money.text = money.ToString();
+        TodayText.text = Today(day);
     }
+
+    private string Today(float day)
+    {
+        float today = day % 7;
+        switch(today)
+        {
+            case 1:
+                return "MON";
+            case 2:
+                return "TUE";
+            case 3:
+                return "WED";
+            case 4:
+                return "THU";
+            case 5:
+                return "FRI";
+            case 6:
+                return "SAT";
+            case 0:
+                return "SUN";
+            default:
+                return "";
+        }
+    }
+
     public void AddObserver(IGameDataOb o)
     {
         GameData.Instance.AddObserver(this);
