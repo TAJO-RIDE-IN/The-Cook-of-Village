352603699adabb7611c5a67bfb5b5133cd3785e0
WIN10PC\User <chahg0000@gmail.com>
2022-07-11 05:45:59 +0900

    아이템 슬롯에 추가까지 구현

--- The Cook of Village/Assets/Scripts/Manager/CHG/ItemSlotManager.cs
+++ The Cook of Village/Assets/Scripts/Manager/CHG/ItemSlotManager.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemSlotManager : MonoBehaviour
 {
     public Text WarningText;
-
+    
+    public Sprite lockedSlot;
+    public Sprite emptySlot;
+    
+    
+    private InventoryManager _inventoryManager;
+    public ItemSlot[] itemslots;
     void Start()
     {
-        
+        itemslots = transform.GetComponentsInChildren<ItemSlot>();
+        _inventoryManager = GameObject.FindWithTag("InventoryManager").GetComponent<InventoryManager>();
+        WarningText.color = new Color(WarningText.color.r, WarningText.color.g, WarningText.color.b, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AddIngredientItem(IngredientsInfos infos)
+    {
+        for (int i = 0; i < _inventoryManager.MaxInven; i++)
+        {
+            if (itemslots[i].isBeingUsed == false)
+            {
+                itemslots[i].isBeingUsed = true;
+                itemslots[i].changeSlotUI(infos.ImageUI);
+                return;
+            }
+        }
+    }
+    public void AddFoodItem(FoodInfos infos)
+    {
+        for (int i = 0; i < _inventoryManager.MaxInven; i++)
+        {
+            if (itemslots[i].isBeingUsed == false)
+            {
+                itemslots[i].isBeingUsed = true;
+                itemslots[i].changeSlotUI(infos.ImageUI);
+                return;
+            }
+        }
     }
 
     public void ShowWarning()
@@ -25,6 +59,7 @@ public class ItemSlotManager : MonoBehaviour
 
     public IEnumerator TextFadeOut()
     {
+        WarningText.color = new Color(WarningText.color.r, WarningText.color.g, WarningText.color.b, 1f);
         //InvenWarning.SetActive(true);
         int loopNum = 0;
         //InvenWarning.color = Color.
