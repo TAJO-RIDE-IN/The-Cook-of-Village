502cbb33a7551218d55c379660389fc9436f241b
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-07-11 02:44:29 +0900

    Feat : 레시피 음식에 맞는 설명 출력

--- The Cook of Village/Assets/Scripts/UI/Naeun/Recipe/SelectRecipe.cs
+++ The Cook of Village/Assets/Scripts/UI/Naeun/Recipe/SelectRecipe.cs
@@ -24,6 +24,7 @@ public class SelectRecipe : MonoBehaviour
         Init();
         int count = 0;
         FoodImage.sprite = foodInfos.ImageUI;
+        FoodExplanation.text = Explanation.FoodExplanation[foodInfos.ID];
         FoodText.text = Localization.GetLocalizedString("Food", foodInfos.Name);
         foreach(int ingredientID in foodInfos.Recipe)
         {
