01a8f8519e7ef090b13d906d3e96171c6a5e2969
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-08-31 23:01:25 +0900

    Feat : 로딩바 기능 추가

--- The Cook of Village/Assets/Scripts/ChangeScene.cs
+++ The Cook of Village/Assets/Scripts/ChangeScene.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     public enum SceneToGo { Intro = 0, SceneLoad = 1, Village = 2, Restaurant = 3 }
-
     public SceneToGo sceneToGo;
+    public Image LoadingBarFill;
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -20,5 +21,16 @@ public class ChangeScene : MonoBehaviour
     {
             GameManager.Instance.NextSceneIndex = (int)sceneToGo;
             SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+
+    private IEnumerator LoadSceneAsync(int sceneID)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while(!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            LoadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
     }
 }
