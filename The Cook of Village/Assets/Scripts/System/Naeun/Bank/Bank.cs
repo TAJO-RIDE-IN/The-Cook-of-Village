b13965edc543cfed94b198168cf58a39d3ae903f
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-09-11 22:48:58 +0900

    Feat : 은행 돈 입력할 때 최대 최소 범위 설정

--- The Cook of Village/Assets/Scripts/System/Naeun/Bank/Bank.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Bank/Bank.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bank : MonoBehaviour
 {
+
+    private enum State { Deposit, Withdrawal }
+    [SerializeField] private State CurrentState;
+
     private int ChangeInterestDay = 3;
-    public void MoneyWithdrawal(int money)
+    public InputField InputMoneyText;
+    public void BankButtonClick()
     {
-        GameData.Instance.BankMoney -= money;
-        GameData.Instance.Money += money;
+        GameData.Instance.Money += MoneyValue();
+        GameData.Instance.BankMoney -= MoneyValue();
     }
-    public void MoneyDeposit(int money)
+    private int MoneyValue()
     {
-        GameData.Instance.BankMoney += money;
-        GameData.Instance.Money -= money;
+        int _money = Int32.Parse(InputMoneyText.text);
+        _money = (CurrentState == State.Deposit) ? -_money : _money; //입금 : 출금
+        return _money;
+    }
+    public void MaxMoneyText()
+    {
+        int MaxMoney = (CurrentState == State.Deposit) ? GameData.Instance.Money : GameData.Instance.BankMoney;
+        if (InputMoneyText.text != null)
+        {
+            int _money = Mathf.Clamp(Int32.Parse(InputMoneyText.text), 0, MaxMoney);
+            InputMoneyText.text = _money.ToString();
+        }
     }
 }
