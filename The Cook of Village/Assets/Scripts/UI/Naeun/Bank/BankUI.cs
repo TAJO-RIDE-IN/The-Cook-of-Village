e9f3bc42ef1d2f762a513e8a6099cc92c1b265a3
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-09-13 15:30:50 +0900

    Feat : 돈 입력시 단위마다 콤마 추가

--- The Cook of Village/Assets/Scripts/UI/Naeun/Bank/BankUI.cs
+++ The Cook of Village/Assets/Scripts/UI/Naeun/Bank/BankUI.cs
@@ -1,14 +1,24 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BankUI : UIController
 {
-    private enum State { Deposit, Withdrawal }
-    [SerializeField] private State CurrentState;
-    public InputField InputMoneyText;
+    public enum Service { Deposit, Withdrawal } //입금, 출금
+    [SerializeField] private Service service;
+    public Service CurrentService
+    {
+        get { return service; }
+        set
+        {
+            service = value;
+            LoadData();
+        }
+    }
+    public InputField MoneyInputField;
+    public Text BankServiceButtonText;
+    public Text BankMoneyText;
 
     public void UIState(bool state)
     {
@@ -16,28 +26,67 @@ public class BankUI : UIController
         if (state) { LoadData(); }
     }
 
-    public void BankButtonClick()
-    {
-        GameData.Instance.Money += MoneyValue();
-        GameData.Instance.BankMoney -= MoneyValue();
-    }
     private int MoneyValue()
     {
-        int _money = Int32.Parse(InputMoneyText.text);
-        _money = (CurrentState == State.Deposit) ? -_money : _money; //입금 : 출금
+        int _money = Int32.Parse(ReplaceMoneyText(MoneyInputField.text));
+        _money = (CurrentService == Service.Deposit) ? -_money : _money; //입금 : 출금
         return _money;
     }
     private void LoadData()
     {
-
-    }
-    public void MaxMoneyText()
-    {
-        if (InputMoneyText.text != null)
+        BankMoneyText.text = MoneyText(GameData.Instance.BankMoney) + "원";
+        MoneyInputField.text = "";
+        switch (CurrentService)
         {
-            int MaxMoney = (CurrentState == State.Deposit) ? GameData.Instance.Money : GameData.Instance.BankMoney;
-            int _money = Mathf.Clamp(Int32.Parse(InputMoneyText.text), 0, MaxMoney);
-            InputMoneyText.text = _money.ToString();
+            case Service.Deposit:
+                BankServiceButtonText.text = "입금하기";
+                break;
+            case Service.Withdrawal:
+                BankServiceButtonText.text = "출금하기";
+                break;
         }
+    }
+    public void ChangeService(int state)
+    {
+        CurrentService = (Service)state;
+    }
+
+    public void BankButtonClick()
+    {
+        GameData.Instance.Money += MoneyValue();
+        GameData.Instance.BankMoney -= MoneyValue();
+        LoadData();
+    }
+
+    public void TotalMoneyButtonClick()
+    {
+        MoneyInputField.text = MoneyText(MaxMoney());
+    }
+
+    private string MoneyText(int _money) //숫자 콤마 표시
+    {
+        return _money.ToString("N0");
+    }
+    private string ReplaceMoneyText(string _text) //문자열에서 숫자만 얻기
+    {
+        string StrMoney = Regex.Replace(_text, @"[^0-9]", "");
+        return StrMoney;
+    }
+
+    public void MoneyTextRange() //입력 범위 조절
+    {
+        string _text = ReplaceMoneyText(MoneyInputField.text);
+        if (MoneyInputField.text != "" && _text != "")
+        {
+            int _money = Mathf.Clamp(Int32.Parse(_text), 0, MaxMoney());
+            MoneyInputField.text = MoneyText(_money);
+            return;
+        }
+        MoneyInputField.text = _text;
+    }
+    private int MaxMoney() //최대 금액
+    {
+        int MaxMoney = (CurrentService == Service.Deposit) ? GameData.Instance.Money : GameData.Instance.BankMoney;
+        return MaxMoney;
     }
 }
