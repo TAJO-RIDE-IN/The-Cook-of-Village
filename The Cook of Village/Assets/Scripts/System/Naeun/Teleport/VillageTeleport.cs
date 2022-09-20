bd1ae14e544b19501009a86f34e131b587054ac7
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-09-20 22:15:19 +0900

    Feat : 파티클 UI위에 나타냄

--- The Cook of Village/Assets/Scripts/System/Naeun/Teleport/VillageTeleport.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Teleport/VillageTeleport.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 
 public class VillageTeleport : MonoBehaviour
 {
-    public static TeleportGate PotionShop;
-    public static TeleportGate PotionShopOut;
-    public static TeleportGate InteriorShop;
-    public static TeleportGate InteriorShopOut;
-    public static TeleportGate Bank;
-    public static TeleportGate BankOut;
+    public Transform PotionShop;
+    public Transform PotionShopOut;
+    public GameObject InteriorShop;
+    public GameObject InteriorShopOut;
+    public GameObject Bank;
+    public GameObject BankOut;
     public ParticleSystem MoveEffect;
     public GameObject Player;
 
-    public Dictionary<TeleportGate, TeleportGate> GateDictionary = new Dictionary<TeleportGate, TeleportGate>
+    public Dictionary<Gate, Transform> GateDictionary = new Dictionary<Gate, Transform>();
+    private void Awake()
     {
-        { PotionShop, PotionShopOut}, {PotionShopOut, PotionShop},
-        {InteriorShop, InteriorShopOut}, {InteriorShopOut, InteriorShop},
-        {Bank, BankOut}, {BankOut, Bank}
-    };
+        GateDictionary.Add(Gate.PotionShop, PotionShopOut);
+        GateDictionary.Add(Gate.PotionShopOut, PotionShop);
+/*        GateDictionary.Add(Gate.InteriorShop, InteriorShopOut);
+        GateDictionary.Add(Gate.InteriorShopOut, InteriorShop);
+        GateDictionary.Add(Gate.Bank, BankOut);
+        GateDictionary.Add(Gate.BankOut, Bank);*/
+    }
+    public enum Gate { PotionShop, PotionShopOut, InteriorShop, InteriorShopOut, Bank, BankOut};
     private void PlayMoveEffect()
     {
+        MoveEffect.gameObject.SetActive(true);
         MoveEffect.Play();
     }
 
-    public void MoveGate(TeleportGate gate)
+    public void MoveGate(Gate gate)
     {
-        Player.transform.position = GateDictionary[gate].gameObject.transform.position;
+        Vector3 TeleportPosition = new Vector3(GateDictionary[gate].position.x + 10, GateDictionary[gate].position.y, GateDictionary[gate].position.z);
+        Debug.Log(TeleportPosition);
+        Player.transform.position = TeleportPosition;
         PlayMoveEffect();
     }
 }
