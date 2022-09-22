32ae251050321b116bcab70d791b8351a0076fc7
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-09-22 16:38:19 +0900

    Feat : 플레이어가 상점으로 텔레포트하는 기능 추가

--- The Cook of Village/Assets/Scripts/System/Naeun/Teleport/VillageTeleport.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Teleport/VillageTeleport.cs
@@ -7,34 +7,35 @@ public class VillageTeleport : MonoBehaviour
 {
     public Transform PotionShop;
     public Transform PotionShopOut;
-    public GameObject InteriorShop;
-    public GameObject InteriorShopOut;
-    public GameObject Bank;
-    public GameObject BankOut;
+    public Transform InteriorShop;
+    public Transform InteriorShopOut;
+    public Transform Bank;
+    public Transform BankOut;
     public ParticleSystem MoveEffect;
-    public GameObject Player;
+    public ThirdPersonGravity Player;
 
     public Dictionary<Gate, Transform> GateDictionary = new Dictionary<Gate, Transform>();
     private void Awake()
     {
         GateDictionary.Add(Gate.PotionShop, PotionShopOut);
         GateDictionary.Add(Gate.PotionShopOut, PotionShop);
-/*        GateDictionary.Add(Gate.InteriorShop, InteriorShopOut);
+        GateDictionary.Add(Gate.InteriorShop, InteriorShopOut);
         GateDictionary.Add(Gate.InteriorShopOut, InteriorShop);
         GateDictionary.Add(Gate.Bank, BankOut);
-        GateDictionary.Add(Gate.BankOut, Bank);*/
+        GateDictionary.Add(Gate.BankOut, Bank);
     }
     public enum Gate { PotionShop, PotionShopOut, InteriorShop, InteriorShopOut, Bank, BankOut};
-    private void PlayMoveEffect()
+    private void PlayMove(Gate gate)
     {
-        MoveEffect.Play();
+        Player.controller.enabled = false;
+        Player.transform.position = GateDictionary[gate].position;
+        Player.transform.rotation = GateDictionary[gate].rotation;
+        Player.controller.enabled = true;
     }
 
     public void MoveGate(Gate gate)
     {
-        Vector3 TeleportPosition = new Vector3(GateDictionary[gate].position.x + 10, GateDictionary[gate].position.y, GateDictionary[gate].position.z);
-        Debug.Log(TeleportPosition);
-        Player.transform.position = TeleportPosition;
-        PlayMoveEffect();
+        MoveEffect.Play();
+        StartCoroutine(ChangeWithDelay.CheckDelay(0.5f, () => PlayMove(gate)));
     }
 }
