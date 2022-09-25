fe385d730a41efb848827e3faf47dd201fb76b53
Jung Naeun <jne02310@gamil.com>
2022-09-25 23:33:36 +0900

    Feat : 캘린더 스크립트 추가

---
+++ The Cook of Village/Assets/Scripts/System/Naeun/Calendar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calendar : MonoBehaviour
+{
+    public GameObject[] DaySlot;
+    public ShopNPC[] ShopNPCHoliday;
+    public BankNPC BankNPCHoliday;
+
+}
