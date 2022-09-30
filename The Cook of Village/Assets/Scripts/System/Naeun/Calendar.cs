d8414950b018cf932e53613be1878dafd619729e
naeun0203 <jne02310@gmail.com>
2022-09-30 22:01:39 +0900

    Feat : 캘린더 날짜에 하이라이트

--- The Cook of Village/Assets/Scripts/System/Naeun/Calendar.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Calendar.cs
@@ -6,17 +6,22 @@ using UnityEngine.UI;
 public class Calendar : MonoBehaviour, IObserver<GameData>
 {
     public Image[] DayImage;
-    public NPCInfos[] NPCHoliday;
-
+    public Image YesterDayImage;
+    public Text MonthText;
     private void Start()
     {
         AddObserver(GameData.Instance);
     }
-
-
+    private void OnEnable()
+    {
+        Dayhighlight(GameData.Instance.Date);
+        MonthText.text = GameData.Instance.Month.ToString() + "¿ù";
+    }
     private void Dayhighlight(int date)
     {
-        DayImage[date - 1].gameObject.SetActive(true);
+        if(YesterDayImage != null) { YesterDayImage.enabled = false; }
+        DayImage[date - 1].enabled = true;
+        YesterDayImage = DayImage[date - 1];
     }
     public void AddObserver(IGameDataOb o)
     {
@@ -28,6 +33,7 @@ public class Calendar : MonoBehaviour, IObserver<GameData>
         if(obj is GameData)
         {
             Dayhighlight(obj.Date);
+            MonthText.text = obj.Month.ToString() + "¿ù";
         }    
     }
 }
