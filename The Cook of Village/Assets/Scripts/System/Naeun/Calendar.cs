4002526883e2f9451dfc986d8995afa5a5f4c53e
naeun0203 <jne02310@gmail.com>
2022-11-22 05:46:08 +0900

    Feat : 캘린더 이미지 업데이트

--- The Cook of Village/Assets/Scripts/System/Naeun/Calendar.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Calendar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Calendar : MonoBehaviour, IObserver<GameData>
+public class Calendar : UIController, IObserver<GameData>
 {
     public Image[] DayImage;
     public Image YesterDayImage;
@@ -12,19 +12,27 @@ public class Calendar : MonoBehaviour, IObserver<GameData>
     {
         AddObserver(GameData.Instance);
     }
-    private void OnEnable()
+    public void CalendarUIState(bool state)
+    {
+        this.gameObject.SetActive(state);
+        if(state)
+        {
+            Init();
+        }
+    }
+    private void Init()
     {
         foreach(var image in DayImage)
         {
-            image.enabled = false;
+            image.gameObject.SetActive(false);
         }
         Dayhighlight(GameData.Instance.Date);
         MonthText.text = GameData.Instance.Month.ToString() + "월";
     }
     private void Dayhighlight(int date)
     {
-        if(YesterDayImage != null) { YesterDayImage.enabled = false; }
-        DayImage[date - 1].enabled = true;
+        if(YesterDayImage != null) { YesterDayImage.gameObject.SetActive(false); }
+        DayImage[date - 1].gameObject.SetActive(true);
         YesterDayImage = DayImage[date - 1];
     }
     public void AddObserver(IGameDataOb o)
