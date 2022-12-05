347366355adbff9f8f43c5531ce6c0c8e4edf085
WIN10PC\User <chahg0000@gmail.com>
2022-12-06 01:04:36 +0900

    투명벽, 쓰레기통 프리팹 변경

--- The Cook of Village/Assets/Scripts/System/CHG/TransOut.cs
+++ The Cook of Village/Assets/Scripts/System/CHG/TransOut.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class TransOut : MonoBehaviour
 {
-    public bool isOut;
     
     public TransIn transIn;
-
     public GameObject transObject;
+
+    
+    public GameObject[] transObjects;
+    public GameObject[] transRevert;
+    
+    public Material[] noTransMaterials;
+    
+    private Material[] _materials;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             if (transIn.isIn)
             {
-                LeanTween.color(transObject, Color.clear, 0.5f);
+                ChangeMaterial();
+                LeanTween.color(transObject, new Color(1, 1, 1, 1f), 0.5f);
+                transIn.isIn = false;
             }
+        }
+    }
+    
+    private void ChangeMaterial()
+    {
+        for (int i = 0; i < transObjects.Length; i++)
+        {
+            transObjects[i].transform.GetComponent<MeshRenderer>().material = noTransMaterials[0];
+        }
+
+        for (int t = 0; t < transRevert.Length; t++)
+        {
+            _materials = transRevert[t].transform.GetComponent<MeshRenderer>().materials;
+            _materials[0] = noTransMaterials[1];
+            _materials[1] = noTransMaterials[0];
+            transRevert[t].transform.GetComponent<MeshRenderer>().materials = _materials;
         }
     }
 
