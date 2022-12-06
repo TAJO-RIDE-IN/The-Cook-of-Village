cb9050392da97e80e6df3f9c2e65d70801f7941f
naeun0203 <jne02310@gmail.com>
2022-12-07 01:12:13 +0900

    Chore : Debug log 삭제

--- The Cook of Village/Assets/Scripts/System/Naeun/Ending/EndingController.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/Ending/EndingController.cs
@@ -29,7 +29,6 @@ public class EndingController : MonoBehaviour, IObserver<GameManager>
         dialogueManager = DialogueManager.Instance;
         soundManager = SoundManager.Instance;
         AddObserver(gameManager);
-        EndingStart();
     }
     /// <summary>
     /// 게임모드가 Ending이 될 경우 다음날 아침에 실행 된다.
@@ -112,7 +111,6 @@ public class EndingController : MonoBehaviour, IObserver<GameManager>
 
         }
         ActionNum++;
-        Debug.Log(ActionNum);
     }
     
     public void FatherAppearance(int index)
