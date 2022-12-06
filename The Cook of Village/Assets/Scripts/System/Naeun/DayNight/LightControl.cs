9d1888505bedd871770969373f432c6d6c23203a
naeun0203 <jne02310@gmail.com>
2022-12-07 00:36:14 +0900

    Feat : 빛 컨트롤러 추가

--- The Cook of Village/Assets/Scripts/System/Naeun/DayNight/LightControl.cs
+++ The Cook of Village/Assets/Scripts/System/Naeun/DayNight/LightControl.cs
@@ -4,26 +4,51 @@ using UnityEngine;
 
 public class LightControl : MonoBehaviour, IObserver<GameData>
 {
-    //public Light
+    public List<Light> ObjectLight = new List<Light>();
     private void Start()
     {
         AddObserver(GameData.Instance);
+    }
+
+    [Range(0.8f, 2.5f)]
+    public float LightIntensity;
+    private bool isMoning = false;
+    private bool isChange = false;
+    private void ChangeIntensity()
+    {
+        LightIntensity += 0.001f;
+        isChange = LightIntensity > 2.5f;
+        ChangeLightBrightness();
+    }
+    private void ChangeLightBrightness()
+    {
+        foreach(var light in ObjectLight)
+        {
+            light.intensity = LightIntensity;
+        }
     }
 
     public void AddObserver(IGameDataOb o)
     {
         o.AddObserver(this);
     }
-    public void RemoveObserver(IGameDataOb o)
-    {
-        if (o != null) { o.RemoveObserver(this); }
-    }
-
     public void Change(GameData obj)
     {
         if (obj is GameData)
         {
             var GameData = obj;
+            if(obj.TimeOfDay > 1100 && obj.TimeOfDay < 1400 && !isChange)
+            {
+                isMoning = false;
+                ChangeIntensity();
+            }
+            if(obj.TimeOfDay > 480 && obj.TimeOfDay < 1000 && !isMoning)
+            {
+                isMoning = true;
+                isChange = false;
+                LightIntensity = 0.8f;
+                ChangeLightBrightness();
+            }
         }
     }
 }
