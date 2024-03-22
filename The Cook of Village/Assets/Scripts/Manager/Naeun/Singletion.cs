f1eadd45cd13ad3822783f3aa84e7384d91b3d31
chahg0000@gmail.com <chahg0000@gmail.com>
2024-03-22 20:33:07 +0900

    chg_0322

--- The Cook of Village/Assets/Scripts/Manager/Naeun/Singletion.cs
+++ The Cook of Village/Assets/Scripts/Manager/Naeun/Singletion.cs
@@ -18,7 +18,7 @@ public class Singletion<T> : MonoBehaviour where T : MonoBehaviour
     }
     private static void AddInstace()
     {
-        var component = FindObjectOfType<T>();
+        var component = FindObjectOfType<T>(); //이게 사용하려면 아예 싱글톤오브젝트 자체를 안만들어놨다가 자동으로 만들게 하려고 넣은건가보네
         if (component == null)
         {
             GameObject obj = new GameObject(typeof(T).Name);
